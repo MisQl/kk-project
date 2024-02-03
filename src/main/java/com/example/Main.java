bd9e9dae57b7c2f189f9ebc638f8747200aa994a
michal.paukszto <michal.paukszto@gmail.com>
2024-02-03 09:55:51 +0100

    hibernate configuration has been added

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import org.hibernate.SessionFactory;
+
 public class Main {
+
     public static void main(String[] args) {
+        SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
         System.out.println("Hello world!");
     }
 }
