579e819bd39b3a863da57e3b4f5bb82bfef2a035
Jakub Heppner <jakub.heppner@gmail.com>
2024-02-04 13:05:13 +0100

    Stworzona encja Person oraz klasa PersonRepository z zawartością.

--- src/main/java/com/example/kuba/Person.java
+++ src/main/java/com/example/kuba/Person.java
@@ -1,15 +1,32 @@
 package com.example.kuba;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
+@Entity
+@Table(name = "Person")
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "age")
     private Integer age;
+    @Column(name = "name")
     private String name;
 
-    public Person(Integer age, String name) {
+    public Person(String name,Integer age) {
         this.age = age;
         this.name = name;
     }
 
     public Person() {
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public Integer getAge() {
@@ -20,11 +37,19 @@ public class Person {
         return name;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setAge(Integer age) {
         this.age = age;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Person{id="+id+"/name="+name+"/age="+age+"} ";
     }
 }
--- src/main/java/com/example/kuba/PersonRepository.java
+++ src/main/java/com/example/kuba/PersonRepository.java
@@ -1,15 +1,21 @@
 package com.example.kuba;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 
 public class PersonRepository {
 
+    @PersistenceContext
+    private EntityManager entityManager;
 public void createPerson(String name, Integer age) {
-
+    Person person = new Person(name, age);
+    entityManager.persist(person);
 }
 
-   public Person findById(Long id) {
+    public Person findById(Long id) {
+        return entityManager.find(Person.class, id);
+    }
 
-       return null;
-   }
 
 
 
