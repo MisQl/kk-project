b06b1f4162a5aab0751bd5007fd263bc9f460a12
Jakub Heppner <jakub.heppner@gmail.com>
2024-02-04 12:22:50 +0100

    Pusty projekt ze stworzonymi wspępnie klasami, bez metod

---
+++ src/main/java/com/example/kuba/Person.java
@@ -0,0 +1,30 @@
+package com.example.kuba;
+
+public class Person {
+    private Integer age;
+    private String name;
+
+    public Person(Integer age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/kuba/PersonRepository.java
@@ -0,0 +1,17 @@
+package com.example.kuba;
+
+public class PersonRepository {
+
+public void createPerson(String name, Integer age) {
+
+}
+
+   public Person findById(Long id) {
+
+       return null;
+   }
+
+
+
+
+}
